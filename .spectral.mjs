e33403f8edbf5c6b927c0dc931ed9ad8aef7125f
bhaskar.sontakke@smartbear.com <bhaskar.sontakke@smartbear.com>
2025-01-08 10:44:31 +0530

    Added 3 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI2ODkwOA/spectral.js?branch_slug=main&token=f5a5a558-e5c1-44fb-978e-caa5c40e38ae";
+export default { extends: ruleset };
